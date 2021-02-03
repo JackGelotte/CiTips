a3ded9212d4e653786496da717f66922a25e8625
Jack Gelotte <jackgelotte@gmail.com>
2021-02-03 13:12:04 +0100

    Sök knapp tar nu value från sökbar.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 console.log('Hello World!');
 
+function search() {
+    let searchBar = document.getElementById('searchBar');
+    let searchString = searchBar.value;
+    alert(searchString);
+}
+
+const searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', search);
