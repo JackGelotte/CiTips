b6d06c3807f86bdc72a4efd3260a9b242c991886
Jack Gelotte <jackgelotte@gmail.com>
2021-02-04 15:05:15 +0100

    Checkboxes, med lite funktion

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-console.log('Hello World!');
+
+const searchBtn = document.getElementById('searchBtn');
 
 function search() {
     let searchBar = document.getElementById('searchBar');
     let searchString = searchBar.value;
-    alert(searchString);
-}
+    console.log(searchString);
 
-const searchBtn = document.getElementById('searchBtn');
+    let c1 = document.getElementById('checkWeather');
+    let c2 = document.getElementById('checkAttraction');
+    let c3 = document.getElementById('checkAlpha');
+
+    let weather = document.querySelector('#weather');
+    let attraction = document.querySelector('#attraction');
+
+    if (c1.checked === true) {
+       weather.style.display = 'flex';
+    } else {
+        weather.style.display = 'none';
+    }
+    if (c2.checked === true) {
+        attraction.style.display = 'flex';
+    } else {
+        attraction.style.display = 'none';
+    }
+
+}
 searchBtn.addEventListener('click', search);
